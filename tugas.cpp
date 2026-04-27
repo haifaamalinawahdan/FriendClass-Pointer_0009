27bfb36798a9e79fef7f3e3b7727de5ea12a86f6
haifaamalinawahdan <haifanew3@gmail.com>
2026-04-27 16:19:16 +0700

    membuat 2 class dan menambahkan member di class LayangLayang

---
+++ tugas.cpp
@@ -0,0 +1,19 @@
+// Buatlah sebuah program yang berisi 2 class dimana terdapat class Layang-Layang, Belah Ketupat.
+// Di setiap class nya berisikan luas dan keliling serta input dan output.
+// Setiap variable memiliki akses private, BangunDatar Belah Ketupat merupakan friend function keliling dari Layang-Layang, dan 
+// semua class hanya dapat diakses menggunakan object.
+
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+class BelahKetupat;
+class LayangLayang
+{
+private:
+    double d1, d2;
+    double sisiA, sisiB;
+
+
+};
